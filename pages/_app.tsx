459a2addd0e6f7fb0cccbb3b18c57bb257e6160b
FooQoo <horseshoes.buzz.17@gmail.com>
2023-10-11 02:39:31 +0900

    cloudflare pagesを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import "../styles/index.css";
+
+export const runtime = "edge";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
